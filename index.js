54f9af5ee68b92c17fe66a2baa219de4f1f6ceab
maxgamesNL <72696183+maxgamesNL@users.noreply.github.com>
2022-10-02 00:40:45 +0200

    scroll effect faster

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bg3 = document.getElementById("bg3");
 bg2.style.left = -bg2.offsetWidth*2 + "px";
 function scrollBg2() {
     if (window.scrollY > 50 && window.scrollY < 800) {
-        if (-bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 6 < 0) {
-            bg2.style.left = -bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 6 + "px";
+        if (-bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 12 < 0) {
+            bg2.style.left = -bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 12 + "px";
         } else if (-bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 6 > 600) {
             bg2.style.left = -bg2.offsetWidth * 1.2 + (window.scrollY - 825) * -6 + "px";
         } else {
