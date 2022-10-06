3383dab4b9fb1c50c01e2d875337ea963ad24295
h126564 <80750283+h126564@users.noreply.github.com>
2022-10-06 10:06:36 +0200

    disabled animations

--- index.js
+++ index.js
@@ -5,19 +5,11 @@ const bg3 = document.getElementById("bg3");
 bg2.style.left = -bg2.offsetWidth*2 + "px";
 function scrollBg2() {
     //change to exponential for better sliding
-    if (window.scrollY > 50 && window.scrollY < 800) {
-        if (-bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 12 < 0) {
-            bg2.style.left = -bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 12 + "px";
-        } else if (-bg2.offsetWidth * 1.2 + (window.scrollY - 50) * 6 > 800) {
-            bg2.style.left = -bg2.offsetWidth * 1.2 + (window.scrollY - 825) * -6 + "px";
-        } else {
-            bg2.style.left = 0 + "px";
-        }
-    }
+    bg2.style.left = Math.sqrt()
 };
 onscroll = function(){
     bg1.style.left = -window.scrollY *4 + "px";
-    scrollBg2();
+    //scrollBg2();
 
 
 
