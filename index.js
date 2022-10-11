9d51d598917d083dda2a9a9a82995f2c38ed01cb
h126564 <80750283+h126564@users.noreply.github.com>
2022-10-11 15:57:36 +0200

    js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ window.scrollY = 0;
 const bg1 = document.getElementById("bg1");
 const bg2 = document.getElementById("bg2");
 const bg3 = document.getElementById("bg3");
-bg2.style.left = -bg2.offsetWidth*2 + "px";
+//bg2.style.left = -bg2.offsetWidth*2 + "px";
 function scrollBg2() {
     //change to exponential for better sliding
-    bg2.style.left = Math.sqrt()
+    //bg2.style.left = Math.sqrt()
 };
 onscroll = function(){
     //bg1.style.left = -window.scrollY *4 + "px";
